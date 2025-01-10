d11fea1575a3aacb7f01f9478df893a65a50f00c
Kayla Gordon <heykaylagordon@gmail.com>
2025-01-10 12:32:26 -0700

    Add DSync example

--- src/appsData.js
+++ src/appsData.js
@@ -10,7 +10,13 @@ const appsData = [
     language: 'TypeScript',
     codeToCopy: 'cd && cd example-apps/typescript-example-applications/typescript-sso-example && code . && nodemon index.ts',
     url: 'http://localhost:8000'
-  }
+  },
+  { 
+    category: 'Directory Sync', 
+    language: 'Node.js',
+    codeToCopy: 'cd && cd example-apps/node-example-applications/node-directory-sync-example && code . && nodemon index.ts',
+    url: 'http://localhost:8000'
+  },
 ]
 
 export default appsData;
