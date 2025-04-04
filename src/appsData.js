0f70155f2bb432089bb01da027252d0a7ae113d6
Kayla Gordon <heykaylagordon@gmail.com>
2025-04-04 13:20:39 -0600

    Add next apps

--- src/appsData.js
+++ src/appsData.js
@@ -23,6 +23,18 @@ const appsData = [
     codeToCopy: 'cd && cd example-apps/react-authkit-example && code . && npm run dev',
     url: 'http://localhost:5173/'
   },
+  { 
+    category: 'AuthKit', 
+    language: 'Next.js',
+    codeToCopy: 'cd && cd example-apps/next-authkit-example && code . && npm run dev',
+    url: 'http://localhost:3000/'
+  },
+  { 
+    category: 'B2B Starter Kit', 
+    language: 'Next.js',
+    codeToCopy: 'cd && cd example-apps/next-b2b-starter-kit && code . && pnpm run dev',
+    url: 'http://localhost:3000/'
+  },
 ]
 
 export default appsData;
